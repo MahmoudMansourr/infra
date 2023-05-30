8247d5e87c5fa5ea8115f9111dbd25ba0c637426
MahmoudMansour <Mahmoud.M.Mansourr@gmail.com>
2023-05-30 20:39:11 +0300

    variables added

---
+++ variables.tf
@@ -0,0 +1,60 @@
+variable "REGION" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "AMI" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+
+variable "CIDR" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "NAME" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "PUBLIC_1_CIDR" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "PUBLIC_2_CIDR" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "PRIVATE_1_CIDR" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "PRIVATE_2_CIDR" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "AVAIL_1" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "AVAIL_2" {
+  type        = string
+  default     = ""
+  description = "description"
+}
